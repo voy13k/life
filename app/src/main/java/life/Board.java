185b098c5af5b4e0b8000eb2f4a9361fdeea7355
Voytek Murawski <voy13k@gmail.com>
2024-05-04 20:10:50 +1000

    Add more javadocs

--- app/src/main/java/life/Board.java
+++ app/src/main/java/life/Board.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 
 public class Board {
 
+    // true means alive, false means dead.
+    // NOTE!!! first index is row number, which is y coordinate,
+    // so the cell address is grid[y][x],
+    // could be confusing.
     private boolean grid[/* rows */][/* cols */];
     private int width;
     private int height;
@@ -13,13 +17,13 @@ public class Board {
         this.width = width;
         this.height = height;
 
-        // true means alive, false means dead.
-        // NOTE!!! first index is row number, which is y coordinate,
-        // so the cell address is grid[y][x],
-        // could be confusing.
         grid = new boolean[height][width];
     }
 
+    /**
+     * Set the provided cells alive
+     * @param seed in the form of "[[a, b], [c, d], ...]"
+     */
     public void seed(String seed) {
         Cell[] cells = Cell.parse(seed);
         Arrays.stream(cells).forEach(c -> {
@@ -30,6 +34,10 @@ public class Board {
         });
     }
 
+    /**
+     * Combine all living cells into one JSON array,
+     * @return a string og the form "[[a, b], [c, d], ...]" with the coordinates of the living cells
+     */
     public String getLiveCells() {
         var liveCells = new ArrayList<Cell>();
         for (int row = 0; row < grid.length; ++row) {
@@ -42,12 +50,28 @@ public class Board {
         return liveCells.toString();
     }
 
+    public void tick() {
+    }
+
+    /**
+     * <p>Print the state of the board, live cells represented by 'X'</p>
+     * e.g. a Board(4,5) with 4 live cells:
+     * <pre>
+     *[  XX ]
+     *[  X  ]
+     *[     ]
+     *[ X   ]
+     *</pre>
+     * <p>x coordinates go left to right</p>
+     * <p>y coordinates go top to bottom,</p>
+     * the board above has live cells equal [[3,3], [4,1], [3,2], [2,4]]
+     */
     @Override
     public String toString() {
         var builder = new StringBuilder();
         Arrays.stream(grid).forEach(row -> {
             builder.append("[");
-            for (boolean c: row) {
+            for (boolean c : row) {
                 builder.append(c ? "X" : " ");
             }
             builder.append("]\n");
