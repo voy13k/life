c997b593c89975779e87667b533afeaf12975fa3
Voytek Murawski <voy13k@gmail.com>
2024-05-06 18:33:03 +1000

    Missed unused import

--- app/src/test/java/life/PositionTests.java
+++ app/src/test/java/life/PositionTests.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
